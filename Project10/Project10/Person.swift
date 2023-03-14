b9140bfd1b6de346e435bbd14da588b16c51b1e8
Tony Capelli <114808357+tony-capelli@users.noreply.github.com>
2023-03-14 15:12:33 +0100

    Project10

---
+++ Project10/Project10/Person.swift
@@ -0,0 +1,20 @@
+//
+//  Person.swift
+//  Project10
+//
+//  Created by Tony Capelli on 14/03/23.
+//
+
+import UIKit
+
+class Person: NSObject {
+
+    var name: String
+    var image: String
+    
+    init(name: String, image: String) {
+        self.name = name
+        self.image = image
+    }
+    
+}
