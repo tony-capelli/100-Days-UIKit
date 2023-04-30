84045b7b4d50a013bc5d994a327f6663ba2a8f1c
Tony Capelli <114808357+tony-capelli@users.noreply.github.com>
2023-04-30 17:38:53 +0200

    Project 12a

---
+++ Project12a/Project10/Person.swift
@@ -0,0 +1,30 @@
+//
+//  Person.swift
+//  Project10
+//
+//  Created by Tony Capelli on 14/03/23.
+//
+
+import UIKit
+
+class Person: NSObject, NSCoding {
+    func encode(with coder: NSCoder) {
+        coder.encode(name, forKey: "name")
+        coder.encode(image, forKey: "image")
+    }
+    
+    required init?(coder: NSCoder) {
+        name = coder.decodeObject(forKey: "name") as? String ?? ""
+        image = coder.decodeObject(forKey: "image") as? String ?? ""
+    }
+    
+
+    var name: String
+    var image: String
+    
+    init(name: String, image: String) {
+        self.name = name
+        self.image = image
+    }
+    
+}
